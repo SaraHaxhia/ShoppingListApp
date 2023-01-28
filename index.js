17f30665146bf1dafa781d5b950657585aef53ee
SaraHaxhia <sara_haxhia@hotmail.com>
2023-01-28 10:15:17 +0000

    Making basic templates

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (event) => {
+  //document.documentElement.classList.toggle("active");
+  console.log("hi");
+});
