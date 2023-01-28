76f2446311c42043ebf37cfa5f5541f651b96905
SaraHaxhia <sara_haxhia@hotmail.com>
2023-01-28 15:36:51 +0000

    Used bootstrap to make basic design

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 console.log("yo");
-
-const button = document.querySelector(".button");
-
-
-      button.addEventListener("click", (event) => {
-        //document.documentElement.classList.toggle("active");
-        document.documentElement.classList.toggle("active");
-      });
