da134ba57e1b314d406ef80ae77c2e2be1daf9ba
SaraHaxhia <sara_haxhia@hotmail.com>
2023-02-01 10:05:41 +0000

    Added modal and scrollable table for list

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+/* Currently when you add grams and cups etc. it just concats the strings
+Could make a cool version where it sums amounts properly
+TODO: Fix that */ 
+
+const liveShoppingList = {}; 
+
+
+
+//TODO: Have a database of types. Only ask someone for a type if not already in database.
+//Could even just have the first time you enter an item, it remembers the type 
+
+
+
+//When someone presses the add to list button on the modal for misc
+//take the values of the three choices 
+//add adjacent html to the table 
+
+const miscModalBtn = document.querySelector("\#misc-modal-btn");
+
+miscModalBtn.addEventListener("click", event => {
+  
+})
