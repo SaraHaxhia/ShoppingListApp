2eb2bd9ae7204829ca5baa32a4bef6f62911e61c
SaraHaxhia <sara_haxhia@hotmail.com>
2023-02-01 14:03:30 +0000

    Made add misc, delete all and delete checked work

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-/* Currently when you add grams and cups etc. it just concats the strings
-Could make a cool version where it sums amounts properly
-TODO: Fix that */ 
+
 /*
 class NewListItem {
   constructor(item,quantity,type){
@@ -16,32 +14,114 @@ class NewListItem {
   }
 }*/
 
-function newListItem(item,quantity,type) {
-  liveShoppingList.push({item, quantity, type});
+
+
+let liveShoppingList = []; 
+
+
+function newListItem(item,quantity,type, checked) {
+  //add items to shopping list
+  liveShoppingList.push({item, quantity, type, checked});
 }
 
-const liveShoppingList = [{item:Banana, quantity:2, type: Fruits}]; 
+//TURN LIST INTO HTML ELEMENTS
+const render = () => {
+  
+  let row, item1, quantity1, type1;
+  const tableBody=document.querySelector("tbody");
+  
+  tableBody.innerHTML=""; //erase current list
+ 
+  //loop through liveShoppingList and add elements
+  for( let i= 0; i<liveShoppingList.length; i++){
+    
+    row = document.createElement("tr");
+    if(liveShoppingList[i].checked) row.classList.add("checked");
+
+    item1 = document.createElement("td");
+    item1.textContent = `${liveShoppingList[i].item}`;
+    row.appendChild(item1);
+    
+    quantity1 = document.createElement("td");
+    quantity1.textContent = `${liveShoppingList[i].quantity}`;
+    row.appendChild(quantity1);
+
+    type1 = document.createElement("td");
+    type1.textContent = `${liveShoppingList[i].type}`;
+    row.appendChild(type1);
+    
+    tableBody.appendChild(row);
+  }
+  enableToggleChecked(); 
+}; 
 
 
 
-//TODO: Have a database of types. Only ask someone for a type if not already in database.
-//Could even just have the first time you enter an item, it remembers the type 
 
-
-
-//When someone presses the add to list button on the modal for misc
-//take the values of the three choices 
-//add adjacent html to the table 
-
+// MAKE MISC ADD MODAL WORK - TO ADD ELEMENTS TO LIST 
 const miscModalBtn = document.querySelector("\#misc-modal-btn");
 
+let itemInput, catInput,qtyInput; 
+miscModalBtn.addEventListener("click", event => {
+  itemInput = document.querySelector("\#item-input");
+  catInput = document.querySelector("\#cat-input");
+  qtyInput = document.querySelector("\#qty-input");
+  newListItem(itemInput.value,qtyInput.value, catInput.value, false);
+  
+  render();
+  //BY RENDERING HERE, WE LOSE ELEMENTS WITH THE CLASS CHECKED... FIX !!!!
+  
+  
+  itemInput.value="";
+  catInput.value="";
+  qtyInput.value="";
+})
 
 
-//When someone clicks on table row tr, toggle class checked for that element 
-const tableRow = document.querySelectorAll("tbody tr");
-[...tableRow].forEach(el => el.addEventListener("click", event=>{
+
+//TOGGLE CHECKED CLASS 
+// in render function because you always want to rerun when you render
+let tableRow;
+let objInShoppingList; 
+function enableToggleChecked() {
+  //When someone clicks on table row tr, toggle class checked for that element 
+  //Make it a function so it works when html elements are changed, runs query selector again 
+  tableRow = document.querySelectorAll("tbody tr");
+  console.log(tableRow);
+  [...tableRow].forEach(el => el.addEventListener("click", event=>{
   event.currentTarget.classList.toggle("checked");
-}) );
+
+  //change checked property in object to true or false 
+
+  objInShoppingList =liveShoppingList.find(obj => obj.item == event.currentTarget.querySelector("td").textContent);
+  //toggle
+  objInShoppingList.checked = objInShoppingList.checked ? false : true; 
+  
+}) )
+}
 
 
 
+
+// DELETE ALL BUTTON
+
+const deleteAllIcon = document.querySelector("\#delete-all-icon");
+deleteAllIcon.addEventListener("click", event => {
+  liveShoppingList = [];
+  render();
+})
+
+//DELETE CHECKED BUTTON 
+const deleteCheckedIcon = document.querySelector("\#delete-checked-icon");
+deleteCheckedIcon.addEventListener("click", event => {
+  liveShoppingList = liveShoppingList.filter(obj=>!obj.checked);
+  
+  render();
+})
+
+
+//Example
+newListItem("Cheese", "one block", "dairy", false); 
+newListItem("Chicken", "1kg", "fresh meat", false); 
+
+render();
