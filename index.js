ca338c666107d7016a24e9efba4c007669d28661
SaraHaxhia <sara_haxhia@hotmail.com>
2023-02-05 15:40:07 +0000

    Autcomplete works

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ class NewListItem {
   }
 }*/
 
+
+
 let liveShoppingList = [];
+let shoppingListDatabase = [];
 
 function newListItem(item, quantity, type, checked) {
   //add items to shopping list
   liveShoppingList.push({ item, quantity, type, checked });
+  shoppingListDatabase.push({item, type});
 }
 
 //TURN LIST INTO HTML ELEMENTS
 const render = () => {
   let row, item1, quantity1, type1;
-  const tableBody = document.querySelector("tbody");
+  const tableBody = document.querySelector("\#table-body");
 
   tableBody.innerHTML = ""; //erase current list
 
@@ -66,19 +70,78 @@ function inputCategories(categories) {
 const miscModalBtn = document.querySelector("#misc-modal-btn");
 
 let itemInput, catInput, qtyInput;
+itemInput = document.querySelector("#item-input");
+catInput = document.querySelector("#cat-input");
+qtyInput = document.querySelector("#qty-input");
+
 miscModalBtn.addEventListener("click", (event) => {
-  itemInput = document.querySelector("#item-input");
-  catInput = document.querySelector("#cat-input");
-  qtyInput = document.querySelector("#qty-input");
+  
   newListItem(itemInput.value, qtyInput.value, catInput.value, false);
 
   render();
-  //BY RENDERING HERE, WE LOSE ELEMENTS WITH THE CLASS CHECKED... FIX !!!!
+  
 
   itemInput.value = "";
   catInput.value = "";
   qtyInput.value = "";
+
+  let successAlert = document.querySelector(".alert-success");
+  successAlert.classList.remove("hidden");
+  setTimeout(() => successAlert.classList.add("hidden"), 1000);
+
 });
+
+
+//Auto-fill type !!!!!!
+
+//start by showing correct matches 
+//then let person select by clicking 
+//then let someone select with buttons and enter button (disable normal use)
+
+
+let searchText, newAutocompleteItem; 
+let autocompleteList = document.querySelector("\#autocomplete-list");
+itemInput.addEventListener("keyup", event => {
+  
+  autocompleteList.innerHTML ="";
+  autocompleteList.classList.add("hidden");
+  
+  searchText=event.target.value.trim().toLowerCase(); 
+  //works with event.target, not event.currenttarget, look into... 
+  if(!searchText==""){
+  
+    shoppingListDatabase.forEach(obj => {
+      if (obj.item.trim().toLowerCase().slice(0,searchText.length)==searchText){
+        newAutocompleteItem= document.createElement("li");
+        newAutocompleteItem.classList.add("newAutocompleteItem");
+        newAutocompleteItem.textContent=obj.item;
+        autocompleteList.appendChild(newAutocompleteItem);
+      }
+    })
+    if(!autocompleteList.innerHTML==""){
+      autocompleteList.classList.remove("hidden");
+    }
+  }
+  selectAutocomplete();
+})
+
+let autocompleteItems; 
+
+function selectAutocomplete() {
+  autocompleteItems=document.querySelectorAll(".newAutocompleteItem");
+  autocompleteItems.forEach(li => {
+    li.addEventListener("click", event => {
+      itemInput.value = event.currentTarget.textContent;
+      catInput.value = shoppingListDatabase.find(obj=>obj.item==event.currentTarget.textContent).type;
+      autocompleteList.classList.add("hidden");
+
+    })
+  })
+}
+
+
+
+
 
 //TOGGLE CHECKED CLASS
 // in render function because you always want to rerun when you render
@@ -138,6 +201,7 @@ function sortList(sortedCat) {
 }
 
 sortIcon.addEventListener("click", () => sortList(sortedCat));
+
 
 //Example
 newListItem("Chicken", "1kg", "Meat", false);
